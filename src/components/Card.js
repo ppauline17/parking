8ace7b4ff5b1f0cd4ea92f2bd665e1bee1630e03
ppauline17 <p.pauline017@gmail.com>
2023-09-16 21:27:03 +0200

    update

--- src/components/Card.js
+++ src/components/Card.js
@@ -12,6 +12,16 @@ function Card (props) {
         props.parking.places = "complet";
     }
 
+    const update_date = new Date(props.parking.derniere_mise_a_jour_base);
+    const options = {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit'
+    };
+    const formattedDate = update_date.toLocaleString('fr-FR', options);
+
     return (
         <div className='col-sm-6 col-lg-4 col-xl-3 g-5'>
             <div className="card p-3 rounded-4 border-0 shadow">
@@ -19,6 +29,7 @@ function Card (props) {
                     <h5 className="card-title mb-3">{props.parking.nom}{icon}</h5>
                     <h6 className="card-subtitle mb-2 text-body-secondary">Places restantes : {props.parking.places}</h6>
                     <ProgressBar taux_doccupation={props.parking.taux_doccupation} />
+                    <div className='actualisation mt-2'>Dernière actualisation : {formattedDate}</div>
                 </div>
             </div>
         </div>
--- src/components/Parkings.js
+++ src/components/Parkings.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import Card from '../components/Card';
-import Clock from '../components/Clock';
 import {fetchData} from '../datas/fetchData';
 
 class Parkings extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: [], // Initialiser l'état data avec la structure appropriée
-            time: null
+            data: [] // Initialiser l'état data avec la structure appropriée
         };
     }
 
@@ -28,25 +26,12 @@ class Parkings extends React.Component {
           .then(data => {
             // Mettre à jour l'état data avec les données de la réponse
             this.setState({ data: data.results });
-      
-            // Mise à jour de l'heure de la dernière actualisation de la base de données
-            const update_date = new Date(data.results[0].derniere_mise_a_jour_base);
-            const options = {
-              year: 'numeric',
-              month: '2-digit',
-              day: '2-digit',
-              hour: '2-digit',
-              minute: '2-digit'
-            };
-            const formattedDate = update_date.toLocaleString('fr-FR', options);
-            this.setState({ time: formattedDate });
         })
     }
 
     render() {
         return (
             <div className='parkings mb-5'>
-                <Clock time={this.state.time} />
                 <div className='row'>
                     {this.state.data.map((parking) => (
                         <Card parking={parking} key={parking.id}/>
